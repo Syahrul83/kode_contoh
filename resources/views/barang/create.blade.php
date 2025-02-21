2204fddd8c60e665a6e70c158e55e69c8fa34949
syahrul anwar <syahrulanwar83@gmail.com>
2025-02-21 14:50:35 +0800

    refactor form layout in create and edit views for better structure and readability

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -1,39 +1,48 @@
-
 @extends('temple.master')
 @section('content')
 
 
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-    <form action="{{ route("barang.store") }}" method="POST">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{ route("barang.store") }}" method="POST">
 
 
-        @csrf
+                    @csrf
 
-        <div class="mb-3 mt-3">
-          <label for="nama" class="form-label">nama barang</label>
-          <input type="text" class="form-control" id="name" placeholder="Enter your name" name="nama_barang">
+                    <div class="mb-3 mt-3">
+                        <label for="nama" class="form-label">nama barang</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter your name" name="nama_barang">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="nama_kontak" class="form-label">nama merek</label>
+                        <input type="text" class="form-control" id="nama_kontak" placeholder="Enter your nickname"
+                            name="nama_merek">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="alamat" class="form-label">harga</label>
+                        <input type="number" class="form-control" id="alamat" placeholder="Enter your address" name="harga">
+                    </div>
+
+                    <div class="mb-3">
+                        <select class="form-control" id="type" name="user_id">
+                            @foreach($users as $user)
+                                <option value="{{ $user->id }}" {{ $user->id == $order->user_id ? 'selected' : '' }}>
+                                    {{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+
+
+
+
+            </div>
         </div>
-
-        <div class="mb-3">
-            <label for="nama_kontak" class="form-label">nama merek</label>
-            <input type="text" class="form-control" id="nama_kontak" placeholder="Enter your nickname" name="nama_merek">
-          </div>
-
-        <div class="mb-3">
-          <label for="alamat" class="form-label">harga</label>
-          <input type="number" class="form-control" id="alamat" placeholder="Enter your address" name="harga">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-
-
-
-
-
-</div>
-</div> 
-</div>
+    </div>
 @endsection
--- resources/views/barang/edit.blade.php
+++ resources/views/barang/edit.blade.php
@@ -1,41 +1,49 @@
-
 @extends('temple.master')
 @section('content')
 
 
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-    <form action="{{ route("barang.update",$qurey->id) }}" method="POST">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{ route("barang.update", $qurey->id) }}" method="POST">
 
 
-        @csrf
-        @method('PUT')
+                    @csrf
+                    @method('PUT')
 
-        <div class="mb-3 mt-3">
-          <label for="nama" class="form-label">nama barang</label>
-          <input type="text" class="form-control" id="nama_barang" placeholder="Enter " name="nama_barang" value="{{ old('nama_barang',$qurey->nama_barang) }}">
+                    <div class="mb-3 mt-3">
+                        <label for="nama" class="form-label">nama barang</label>
+                        <input type="text" class="form-control" id="nama_barang" placeholder="Enter " name="nama_barang"
+                            value="{{ old('nama_barang', $qurey->nama_barang) }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="nama_merek" class="form-label">nama panggilan</label>
+                        <input type="text" class="form-control" id="nama_merek" placeholder="Enter " name="nama_merek"
+                            value="{{ old('nama_merek', $qurey->nama_merek) }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="harga" class="form-label">harga</label>
+                        <input type="text" class="form-control" id="harga" placeholder="Enter " name="harga"
+                            value="{{ old('harga', $qurey->harga) }}">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-control" id="type" name="user_id">
+                            @foreach($users as $user)
+                                <option value="{{ $user->id }}" {{ $user->id == $order->user_id ? 'selected' : '' }}>
+                                    {{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+
+
+
+
+            </div>
         </div>
-
-        <div class="mb-3">
-            <label for="nama_merek" class="form-label">nama panggilan</label>
-            <input type="text" class="form-control" id="nama_merek" placeholder="Enter " name="nama_merek" value="{{ old('nama_merek',$qurey->nama_merek) }}">
-          </div>
-
-        <div class="mb-3">
-          <label for="harga" class="form-label">harga</label>
-          <input type="text" class="form-control" id="harga" placeholder="Enter " name="harga" value="{{ old('harga',$qurey->harga) }}">
-        </div>
-
-       
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-
-
-
-
-
-</div>
-</div> 
-</div>
+    </div>
 @endsection
